643127f6bc07d9de50b6a79f44291f2c18278d35
Lucile Bouchire <l.bouchire@gmail.com>
2023-07-18 19:32:23 +0200

    Initialize fatiope-invest project with react

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Logo from '../assets/logo-fiatope-invest.png';
+
+function Home() {
+    return (
+        <div>
+            Bienvenue sur le site de Fiatope invest
+            <img src={Logo} alt="logo de Fiatope Invest" />
+        </div>
+    );
+}
+
+export default Home;
