b87a5aed25e2f55531ea549d0a9b736969795eb7
Lucile Bouchire <l.bouchire@gmail.com>
2023-07-19 10:47:04 +0200

    Creating Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,34 @@
+// FOOTER COMPONENT
+
+function Footer() {
+    return (
+        <footer>
+            <ul>
+                <li>
+                    <i class="icofont-phone"></i>
+                    <a href="tel:33 078 437 358 4" target="_blank">
+                        33 078 437 358 4
+                    </a>
+                </li>
+                <li>
+                    <i class="icofont-envelope"></i>
+                    <a href="mailto:contact.invest@fiatope.com" target="_blank">
+                        contact.invest@fiatope.com
+                    </a>
+                </li>
+                <li>
+                    <i class="icofont-google-map"></i>
+                    <a
+                        href="https://www.google.com/maps/place/23+Av.+Lombart,+92260+Fontenay-aux-Roses,+France/@48.7880179,2.2930128,17z/data=!3m1!4b1!4m6!3m5!1s0x47e670b8133f615f:0x683a309c9f1a90b7!8m2!3d48.7880179!4d2.2930128!16s%2Fg%2F11cpnp3k2g?entry=ttu"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        23 Avenue Lombart 92260 Fontenay-aux-Roses, France
+                    </a>
+                </li>
+            </ul>
+        </footer>
+    );
+}
+
+export default Footer;
