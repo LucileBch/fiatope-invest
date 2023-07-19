367b1738256ec179be8b5d4f99aaf348ceb8e7dd
Lucile Bouchire <l.bouchire@gmail.com>
2023-07-19 15:30:07 +0200

    Adding Fontawesome icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,27 +1,45 @@
 // FOOTER COMPONENT
 
+// Importing Fontawesome for adding icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faPhoneVolume } from '@fortawesome/free-solid-svg-icons';
+import { faHouseChimney } from '@fortawesome/free-solid-svg-icons';
+
+// Importing CSS Header stylesheet
+import '../styles/Footer.css';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
+
 function Footer() {
     return (
-        <footer>
+        <footer className="fiatope-footer">
             <ul>
                 <li>
-                    <i class="icofont-phone"></i>
-                    <a href="tel:33 078 437 358 4" target="_blank">
+                    <FontAwesomeIcon icon={faPhoneVolume} />
+                    <a
+                        href="tel:33 078 437 358 4"
+                        target="_blank"
+                        className="fiatope-footer-element"
+                    >
                         33 078 437 358 4
                     </a>
                 </li>
                 <li>
-                    <i class="icofont-envelope"></i>
-                    <a href="mailto:contact.invest@fiatope.com" target="_blank">
+                    <FontAwesomeIcon icon={faEnvelope} />
+                    <a
+                        href="mailto:contact.invest@fiatope.com"
+                        target="_blank"
+                        className="fiatope-footer-element"
+                    >
                         contact.invest@fiatope.com
                     </a>
                 </li>
                 <li>
-                    <i class="icofont-google-map"></i>
+                    <FontAwesomeIcon icon={faHouseChimney} />
                     <a
                         href="https://www.google.com/maps/place/23+Av.+Lombart,+92260+Fontenay-aux-Roses,+France/@48.7880179,2.2930128,17z/data=!3m1!4b1!4m6!3m5!1s0x47e670b8133f615f:0x683a309c9f1a90b7!8m2!3d48.7880179!4d2.2930128!16s%2Fg%2F11cpnp3k2g?entry=ttu"
                         target="_blank"
-                        rel="noopener noreferrer"
+                        className="fiatope-footer-element"
                     >
                         23 Avenue Lombart 92260 Fontenay-aux-Roses, France
                     </a>
