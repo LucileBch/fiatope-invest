32151d69dd33bd2825c6b337968f1166b354b12f
Lucile Bouchire <l.bouchire@gmail.com>
2023-07-19 10:49:13 +0200

    Finalizing the router tree

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,26 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from '../pages/Home';
+// REACT APP ROUTER COMPONENT
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Importing pages to build the routes
+import Home from '../pages/Home';
+import About from '../pages/About';
+import Angels from '../pages/Angels';
+import Projects from '../pages/Projects';
+
+// function defining the tree of routes
 function App() {
     return (
         <BrowserRouter>
             <Routes>
+                {/* Path to go to home page */}
                 <Route path="/" element={<Home />} />
+                {/* Path to go to who we are page */}
+                <Route path="/about" element={<About />} />
+                {/* Path to go to fiatope angels page */}
+                <Route path="/angels" element={<Angels />} />
+                {/* Path to go to projects page */}
+                <Route path="/projects" element={<Projects />} />
             </Routes>
         </BrowserRouter>
     );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// Importing general main styles
 import './styles/index.css';
+
+// Importing the router component
 import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
