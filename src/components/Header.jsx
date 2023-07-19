85552be458183438731f66d061fec70c5ef20bae
Lucile Bouchire <l.bouchire@gmail.com>
2023-07-19 10:47:20 +0200

    Creating Heder component

---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+// HEADER COMPONENT
+
+// Importing element to able the navigation between pages
+import { NavLink } from 'react-router-dom';
+
+// Importing assets
+import Logo from '../assets/logo-fiatope-invest.png';
+
+function Header() {
+    return (
+        <header>
+            <img src={Logo} alt="logo de fiatope invest" />
+            <ul>
+                <li>
+                    <NavLink to="/">Accueil</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/about">Qui sommes-nous</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/angels">Fiatope Angels</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/projects">Projets</NavLink>
+                </li>
+            </ul>
+        </header>
+    );
+}
+
+export default Header;
