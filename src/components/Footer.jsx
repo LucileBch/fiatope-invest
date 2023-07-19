ce1f8e953834b3d70b9eb18990081a006a8bbd2d
Lucile Bouchire <l.bouchire@gmail.com>
2023-07-19 15:56:26 +0200

    Adding classNames to implement CSS Footer style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 // FOOTER COMPONENT
 
-// Importing Fontawesome for adding icons
+// Importing Fontawesome for inserting icons
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faPhoneVolume } from '@fortawesome/free-solid-svg-icons';
@@ -13,9 +13,12 @@ import { faHouse } from '@fortawesome/free-solid-svg-icons';
 function Footer() {
     return (
         <footer className="fiatope-footer">
-            <ul>
-                <li>
-                    <FontAwesomeIcon icon={faPhoneVolume} />
+            <ul className="fiatope-footer-list">
+                <li className="fiatope-footer-bloc">
+                    <FontAwesomeIcon
+                        icon={faPhoneVolume}
+                        className="fiatope-footer-icon"
+                    />
                     <a
                         href="tel:33 078 437 358 4"
                         target="_blank"
@@ -24,8 +27,11 @@ function Footer() {
                         33 078 437 358 4
                     </a>
                 </li>
-                <li>
-                    <FontAwesomeIcon icon={faEnvelope} />
+                <li className="fiatope-footer-bloc">
+                    <FontAwesomeIcon
+                        icon={faEnvelope}
+                        className="fiatope-footer-icon"
+                    />
                     <a
                         href="mailto:contact.invest@fiatope.com"
                         target="_blank"
@@ -34,14 +40,17 @@ function Footer() {
                         contact.invest@fiatope.com
                     </a>
                 </li>
-                <li>
-                    <FontAwesomeIcon icon={faHouseChimney} />
+                <li className="fiatope-footer-bloc">
+                    <FontAwesomeIcon
+                        icon={faHouseChimney}
+                        className="fiatope-footer-icon"
+                    />
                     <a
                         href="https://www.google.com/maps/place/23+Av.+Lombart,+92260+Fontenay-aux-Roses,+France/@48.7880179,2.2930128,17z/data=!3m1!4b1!4m6!3m5!1s0x47e670b8133f615f:0x683a309c9f1a90b7!8m2!3d48.7880179!4d2.2930128!16s%2Fg%2F11cpnp3k2g?entry=ttu"
                         target="_blank"
                         className="fiatope-footer-element"
                     >
-                        23 Avenue Lombart 92260 Fontenay-aux-Roses, France
+                        23 Avenue Lombart, 92260 Fontenay-aux-Roses, France
                     </a>
                 </li>
             </ul>
