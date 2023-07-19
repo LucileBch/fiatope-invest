6edfb1650032db1d53f5a01bb8987f132fbd7ac3
Lucile Bouchire <l.bouchire@gmail.com>
2023-07-19 10:49:36 +0200

    Inserting all elements of Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,35 @@
-import Logo from '../assets/logo-fiatope-invest.png';
+// HOME PAGE
+
+// Importing components
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+// Importing assets
+import MainPicture from '../assets/image-accueil-fiatope.jpeg';
 
 function Home() {
     return (
-        <div>
-            Bienvenue sur le site de Fiatope invest
-            <img src={Logo} alt="logo de Fiatope Invest" />
-        </div>
+        <body>
+            <Header />
+            <main>
+                <img
+                    src={MainPicture}
+                    alt="groupe de personnes travaillant autour d'une table"
+                />
+                <h1>Nous faisons prospérer votre entreprise !</h1>
+                <h2>Notre mission</h2>
+                <p>
+                    Nous sommes spécialisés dans le financement et
+                    l'accompagnement de petites startups africaines à fort
+                    potentiel de croissance. Nous avons pour objectif d'aider
+                    les porteurs de projets à réaliser leur vision en leur
+                    apportant un financement adapté à leur développement, ainsi
+                    qu'un accompagnement personnalisé tout au long de leur
+                    parcours.
+                </p>
+            </main>
+            <Footer />
+        </body>
     );
 }
 
