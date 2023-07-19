3adbd137371868033fd43b328b8a4cfdad7ce66f
Lucile Bouchire <l.bouchire@gmail.com>
2023-07-19 17:08:50 +0200

    Creating Home page CSS style sheet

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,22 +14,30 @@ function Home() {
     return (
         <body>
             <Header />
-            <main>
-                <img
-                    src={MainPicture}
-                    alt="groupe de personnes travaillant autour d'une table"
-                />
-                <h1>Nous faisons prospérer votre entreprise !</h1>
-                <h2>Notre mission</h2>
-                <p>
-                    Nous sommes spécialisés dans le financement et
-                    l'accompagnement de petites startups africaines à fort
-                    potentiel de croissance. Nous avons pour objectif d'aider
-                    les porteurs de projets à réaliser leur vision en leur
-                    apportant un financement adapté à leur développement, ainsi
-                    qu'un accompagnement personnalisé tout au long de leur
-                    parcours.
-                </p>
+            <main className="fiatope-home-main">
+                <section className="fiatope-home-presentation">
+                    <img
+                        src={MainPicture}
+                        alt="groupe de personnes travaillant autour d'une table"
+                        className="fiatope-home-image"
+                    />
+                    <h1 className="fiatope-home-title">
+                        Nous faisons prospérer votre entreprise !
+                    </h1>
+                </section>
+                <section className="fiatope-home-informations">
+                    <h2>Notre mission</h2>
+                    <p>
+                        Nous sommes spécialisés dans le financement et
+                        l'accompagnement de petites startups africaines à fort
+                        potentiel de croissance. Nous avons pour objectif
+                        d'aider les porteurs de projets à réaliser leur vision
+                        en leur apportant un financement adapté à leur
+                        développement, ainsi qu'un accompagnement personnalisé
+                        tout au long de leur parcours.
+                    </p>
+                    <div className="informations-background"></div>
+                </section>
             </main>
             <Footer />
         </body>
