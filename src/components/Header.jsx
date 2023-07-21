28eb1772d1041dfc4511d1cbac7d6614fa5977e2
Lucile Bouchire <l.bouchire@gmail.com>
2023-07-21 13:17:05 +0200

    Cleaning comments

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,16 @@
 // Importing element to able the navigation between pages
 import { NavLink } from 'react-router-dom';
 
+// Importing Burger component
+import Burger from '../components/Burger.jsx';
+
 // Importing CSS Header stylesheet
 import '../styles/Header.css';
 
 // Importing assets
 import Logo from '../assets/logo-fiatope-invest.png';
 
+// Header function displaying the header website with logo and navigation
 function Header() {
     return (
         <header className="fiatope-header">
@@ -19,56 +23,7 @@ function Header() {
                     className="fiatope-header-logo"
                 />
             </NavLink>
-            <ul className="fiatope-header-list">
-                <li>
-                    <NavLink
-                        to="/"
-                        className={({ isActive }) =>
-                            isActive
-                                ? 'fiatope-link-active'
-                                : 'fiatope-link-inactive'
-                        }
-                    >
-                        Accueil
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        to="/about"
-                        className={({ isActive }) =>
-                            isActive
-                                ? 'fiatope-link-active'
-                                : 'fiatope-link-inactive'
-                        }
-                    >
-                        Qui sommes-nous ?
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        to="/angels"
-                        className={({ isActive }) =>
-                            isActive
-                                ? 'fiatope-link-active'
-                                : 'fiatope-link-inactive'
-                        }
-                    >
-                        Fiatope Angels
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        to="/projects"
-                        className={({ isActive }) =>
-                            isActive
-                                ? 'fiatope-link-active'
-                                : 'fiatope-link-inactive'
-                        }
-                    >
-                        Nos Projets
-                    </NavLink>
-                </li>
-            </ul>
+            <Burger />
         </header>
     );
 }
