430e99f101becff8db3a352c481c483343b88cc4
Lucile Bouchire <l.bouchire@gmail.com>
2023-07-24 08:57:25 +0200

    Adding anchor and state hook to manage condition of display and navigation + adjusting style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,61 +1,79 @@
-// Importing element to able navigation between pages
-import { NavLink } from 'react-router-dom';
+// NAVIGATION BAR COMPONENT
+
+// Importing hook
+import { useState } from 'react';
 
 // Importing Navbar style sheet
 import '../styles/Navbar.css';
 
 // Navbar function displaying navigation menu
-// As a child of Burger component, calling prop open in order to put condition on mobile menu display
+// As a child of Burger component, calling prop "open" to put a display condition on mobile menu display
 function Navbar({ open }) {
+    // Local state checking the link's index
+    const [activeLink, setActiveLink] = useState(null);
+
+    // Function handling click on link
+    const handleLinkClick = (index) => {
+        setActiveLink(index);
+    };
+
     return (
         <ul className={`fiatope-header-list ${open ? '' : 'closed'}`}>
             <li>
-                <NavLink
-                    to="/"
-                    className={({ isActive }) =>
-                        isActive
+                <a
+                    href="#"
+                    className={
+                        activeLink === 0
                             ? 'fiatope-link-active'
                             : 'fiatope-link-inactive'
                     }
+                    // when the link is clicked, the state is updated with index 0
+                    onClick={() => handleLinkClick(0)}
                 >
                     Accueil
-                </NavLink>
+                </a>
             </li>
             <li>
-                <NavLink
-                    to="/about"
-                    className={({ isActive }) =>
-                        isActive
+                <a
+                    href="#section-nous"
+                    className={
+                        activeLink === 1
                             ? 'fiatope-link-active'
                             : 'fiatope-link-inactive'
                     }
+                    // when the link is clicked, the state is updated with index 1
+                    onClick={() => handleLinkClick(1)}
                 >
                     Qui sommes-nous ?
-                </NavLink>
+                </a>
             </li>
             <li>
-                <NavLink
-                    to="/angels"
-                    className={({ isActive }) =>
-                        isActive
+                <a
+                    href="#section-angels"
+                    className={
+                        activeLink === 2
                             ? 'fiatope-link-active'
                             : 'fiatope-link-inactive'
                     }
+                    // when the link is clicked, the state is updated with index 2
+                    onClick={() => handleLinkClick(2)}
                 >
                     Fiatope Angels
-                </NavLink>
+                </a>
             </li>
             <li>
-                <NavLink
-                    to="/projects"
-                    className={({ isActive }) =>
-                        isActive
+                <a
+                    href="#section-projects"
+                    className={
+                        activeLink === 3
                             ? 'fiatope-link-active'
                             : 'fiatope-link-inactive'
                     }
+                    // when the link is clicked, the state is updated with index 3
+                    onClick={() => handleLinkClick(3)}
                 >
                     Nos Projets
-                </NavLink>
+                </a>
             </li>
         </ul>
     );
