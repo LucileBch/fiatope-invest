8618476c06d6d3ea4e6251e75b6632148d3d26ad
Lucile Bouchire <l.bouchire@gmail.com>
2023-07-24 08:55:39 +0200

    Changing Home page in Home section component

---
+++ src/components/Home.jsx
@@ -0,0 +1,40 @@
+// HOME SECTION COMPONENT
+
+// Importing assets
+import MainPicture from '../assets/image-accueil-fiatope.jpeg';
+
+// Importing CSS Home Page stylesheet
+import '../styles/Home.css';
+
+// Home function displaying the home section elements
+function Home() {
+    return (
+        <section id="#" className="fiatope-home-main">
+            <div className="fiatope-home-presentation">
+                <img
+                    src={MainPicture}
+                    alt="groupe de personnes travaillant autour d'une table"
+                    className="fiatope-home-image"
+                />
+                <h1 className="fiatope-home-title">
+                    Nous faisons prospérer votre entreprise !
+                </h1>
+            </div>
+            <div className="fiatope-home-informations">
+                <h2>Notre mission</h2>
+                <p>
+                    Nous sommes spécialisés dans le financement et
+                    l'accompagnement de petites startups africaines à fort
+                    potentiel de croissance. Nous avons pour objectif d'aider
+                    les porteurs de projets à réaliser leur vision en leur
+                    apportant un financement adapté à leur développement, ainsi
+                    qu'un accompagnement personnalisé tout au long de leur
+                    parcours.
+                </p>
+                <div className="informations-background"></div>
+            </div>
+        </section>
+    );
+}
+
+export default Home;
