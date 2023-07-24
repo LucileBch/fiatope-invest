ffc7d965ee055e4355e34738526cc93852a62264
Lucile Bouchire <l.bouchire@gmail.com>
2023-07-24 11:21:00 +0200

    Adding Value section in the main page

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -3,6 +3,7 @@
 // Importing components to build the page display
 import Header from '../components/Header';
 import Home from '../components/Home';
+import Value from '../components/Value';
 import Projects from '../components/Projects';
 import Footer from '../components/Footer';
 
@@ -12,6 +13,7 @@ function App() {
         <div>
             <Header />
             <Home />
+            <Value />
             <Projects />
             <Footer />
         </div>
