1ce7e0a9acc843103291949797338f3c0834ddf7
Lucile Bouchire <l.bouchire@gmail.com>
2023-07-24 09:46:00 +0200

    Importing stylesheet

--- src/components/CardProject.jsx
+++ src/components/CardProject.jsx
@@ -1,15 +1,18 @@
 // CARDPROJECT COMPONENT
 
+// Importing CSS CardProject stylesheet
+import '../styles/CardProject.css';
+
 // Importing component
 import Button from '../components/Button';
 
 // Cardproject function in order to add other projects in the future
 function CardProject({ content, image }) {
     return (
-        <article>
+        <article className="project-article">
             {image}
             <p>{content}</p>
-            {/* MISSING LINK FOR KNOW */}
+            {/* MISSING LINK FOR NOW */}
             <Button label="Découvrir" href="#" />
         </article>
     );
