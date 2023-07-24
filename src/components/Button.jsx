304359f16fd147bc3b35726e863437b8117cc85f
Lucile Bouchire <l.bouchire@gmail.com>
2023-07-24 09:15:45 +0200

    Correcting button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,25 +2,27 @@
 
 // Importing
 
-// Button function to be re-usable for all buttons
-function Button({ label, onClick, href, download }) {
+// Button function using props to be re-usable for all buttons
+function Button({ label, href, download, onClick }) {
+    // If prop "href", display a button in <a> tag, sending to the specified URL
     if (href) {
+        // If prop "download" is also provided, display a button in <a> tag, allowing to download the linked document
+        if (download) {
+            return (
+                <a href={download}>
+                    <button>{label}</button>
+                </a>
+            );
+        }
+        // If only "href" is provided, display a regular link
         return (
             <a href={href}>
                 <button>{label}</button>
             </a>
         );
-
-        if (download) {
-            return (
-                <a href={download}>
-                    {' '}
-                    <button>{label}</button>
-                </a>
-            );
-        }
-        return <button onClick={onClick}> {label} </button>;
     }
+    // If neither "href" nor "download" is provided, display a regular button with onClick event handler
+    return <button onClick={onClick}>{label}</button>;
 }
 
 export default Button;
