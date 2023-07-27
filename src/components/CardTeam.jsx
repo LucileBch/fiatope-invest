723939561541cab37439d3941a65ccc36a79d388
Lucile Bouchire <l.bouchire@gmail.com>
2023-07-27 18:28:17 +0200

    Creating a component for team members

---
+++ src/components/CardTeam.jsx
@@ -0,0 +1,12 @@
+// CARDTEAM COMPONENT
+
+function CardTeam({ image, name }) {
+    return (
+        <article>
+            {image}
+            <h3>{name}</h3>
+        </article>
+    );
+}
+
+export default CardTeam;
